915116acbe56126b8b8f8acd459be54edc6b49f9
toast <cpastoast@protonmail.com>
2022-05-05 12:03:41 +0200

    update

--- squiz/logger.py
+++ squiz/logger.py
@@ -1,8 +1,4 @@
 from rich.console import Console
-from rich.panel import Panel
-from rich.box import HEAVY
-
-from typing import Iterable
 
 from squiz import __version__
 
@@ -38,27 +34,6 @@ class Logger:
     ) -> None:
         """Logs a message to the console"""
         console.print(f"[bold {color}]{title}[/] [bold]{message}[/]")
-
-    @classmethod
-    def pretty_dict(cls, d: dict) -> Panel:
-        """Render a dict as a rich object"""
-
-        def f() -> Iterable[str]:
-            """Render the model as a string with 'key : value'"""
-            max_len = max(map(len, d.keys()))
-
-            for key, value in d.items():
-
-                if not value:
-                    continue
-
-                yield (f"{key:<{max_len}} : {value}\n")
-
-        return Panel.fit(
-            "".join(f()),
-            border_style="bright_black",
-            box=HEAVY,
-        )
 
     @classmethod
     def error(cls, message: str) -> None:
